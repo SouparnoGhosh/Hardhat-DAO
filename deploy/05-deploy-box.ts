7adbbc5ec08a0d78ce309384cdca0720e7e04e4b
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-07-04 14:52:20 +0530

    deploy box

--- deploy/05-deploy-box.ts
+++ deploy/05-deploy-box.ts
@@ -1,3 +1,5 @@
+/* eslint-disable node/no-unpublished-import */
+/* eslint-disable node/no-missing-import */
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import verify from "../helper-hardhat-functions";
@@ -11,4 +13,28 @@ const deployBox: DeployFunction = async function (
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   log("----------------------------------------------------");
+  log("Deploying Box and waiting for confirmations...");
+  const box = await deploy("Box", {
+    from: deployer,
+    args: [],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  });
+  log(`Box at ${box.address}`);
+  // Verification
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(box.address, []);
+  }
+  const boxContract = await ethers.getContractAt("Box", box.address);
+  const timeLock = await ethers.getContract("TimeLock");
+  const transferTx = await boxContract.transferOwnership(timeLock.address);
+  await transferTx.wait(1);
+  log("----------------------------------------------------");
 };
+
+export default deployBox;
+deployBox.tags = ["all", "box"];
