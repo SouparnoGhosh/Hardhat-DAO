ad9e36dc3e0a613408f503caccfa21ecb95b009b
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-07-04 13:48:12 +0530

    added deploy governor token

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -15,6 +15,40 @@ const deployGovernanceToken: DeployFunction = async function (
   const { deployer } = await getNamedAccounts();
   log("----------------------------------------------------");
   log("Deploying GovernanceToken and waiting for confirmations...");
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  });
+  log(`GovernanceToken at ${governanceToken.address}`);
+  // Verification
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(governanceToken.address, []);
+  }
+  // Delegating to deployer
+  log("----------------------------------------------------");
+  log(`Delegating to ${deployer}`);
+  await delegate(governanceToken.address, deployer);
+  log("Delegated!");
+};
+
+const delegate = async (
+  governanceTokenAddress: string,
+  delegatedAccount: string
+) => {
+  const governanceToken = await ethers.getContractAt(
+    "GovernanceToken",
+    governanceTokenAddress
+  );
+  const transactionResponse = await governanceToken.delegate(delegatedAccount);
+  await transactionResponse.wait(1);
+  console.log(
+    `Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`
+  );
 };
 
 export default deployGovernanceToken;
